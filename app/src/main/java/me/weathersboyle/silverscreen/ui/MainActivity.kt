bc36008762a271b3fa2bb74903aea60ed89ed5ac
Boyle, Rob <rob.boyle@accenture.com>
2019-06-04 13:46:45 -0400

    init

---
+++ app/src/main/java/me/weathersboyle/silverscreen/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.weathersboyle.silverscreen.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.weathersboyle.silverscreen.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
